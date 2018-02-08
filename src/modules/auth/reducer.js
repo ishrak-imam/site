707d9fb17edc6c15ffe4f8688c061373c032f787
ishrak <ishrak.imam@gmail.com>
2018-02-09 01:19:13 +0600

    clear store on logout

--- src/modules/auth/reducer.js
+++ src/modules/auth/reducer.js
@@ -2,9 +2,7 @@
 import {
   INIT,
   LOGIN_REQ, LOGIN_SUCS, LOGIN_FAIL,
-  SIGN_UP_REQ, SIGN_UP_SUCS, SIGN_UP_FAIL,
-  LOGOUT_SUCS, LOGOUT_FAIL
-
+  SIGN_UP_REQ, SIGN_UP_SUCS, SIGN_UP_FAIL
 } from './action';
 
 import {createReducer} from '../../utils/redux';
@@ -25,10 +23,7 @@ export const login = createReducer(LOGIN, {
   [INIT]: (state, payload) => ({...state, checking: true}),
   [LOGIN_REQ]: (state, payload) => ({...state, loading: true, error: null}),
   [LOGIN_SUCS]: (state, payload) => ({...state, loading: false, checking: false, user: payload}),
-  [LOGIN_FAIL]: (state, payload) => ({...state, loading: false, checking: false, error: payload}),
-
-  [LOGOUT_SUCS]: (state, payload) => (LOGIN),
-  [LOGOUT_FAIL]: (state, payload) => (LOGIN)
+  [LOGIN_FAIL]: (state, payload) => ({...state, loading: false, checking: false, error: payload})
 });
 
 export const signUp = createReducer(SIGN_UP, {
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,11 +1,24 @@
+
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
+import {
+  LOGOUT_SUCS, LOGOUT_FAIL
+} from '../modules/auth/action';
+
 import * as authReducers from '../modules/auth/reducer';
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   form: formReducer,
   ...authReducers
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === LOGOUT_SUCS || action.type === LOGOUT_FAIL) {
+    console.log(action);
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
 
 export default rootReducer;
